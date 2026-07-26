a72b84a6a3b222b555edc7a597c0dff916d4ab86
Israel <israel.franca895@gmail.com>
2026-07-26 16:30:25 +0000

    implementa logica de contagem, micro-paradas e reset em python

--- src/main.py
+++ src/main.py
@@ -1,1 +1,55 @@
 print("Sistema Kanban Inicializado")
+
+import time
+from machine import Pin, ADC
+
+ldr = ADC(Pin(34))
+ldr.atten(ADC.ATTN_11DB)  
+
+btn = Pin(25, Pin.IN, Pin.PULL_UP)
+
+contador_pecas = 0
+estado_anterior_bloqueado = False
+tempo_bloqueio_inicio = 0
+micro_parada_registrada = False
+
+print("Contador de Producao Inicializado")
+
+
+ultimo_estado_btn = btn.value()
+ultimo_tempo_debounce = time.ticks_ms()
+
+while True:
+    current_time = time.ticks_ms()
+    
+    lux_val = ldr.read() 
+
+    bloqueado = (lux_val < 100)
+    
+    if bloqueado:
+        if not estado_anterior_bloqueado:
+            estado_anterior_bloqueado = True
+            tempo_bloqueio_inicio = current_time
+            micro_parada_registrada = False
+        else:
+            if not micro_parada_registrada and (time.ticks_diff(current_time, tempo_bloqueio_inicio) > 5000):
+                print("Alerta: Micro-parada detectada!")
+                micro_parada_registrada = True
+    else:
+        if estado_anterior_bloqueado:
+            contador_pecas += 1
+            print(f"Peca detectada! Total: {contador_pecas}")
+            estado_anterior_bloqueado = False
+
+    btn_val = btn.value()
+    if btn_val != ultimo_estado_btn:
+        ultimo_tempo_debounce = current_time
+        ultimo_estado_btn = btn_val
+
+    if time.ticks_diff(current_time, ultimo_tempo_debounce) > 50:
+        if btn_val == 0:
+            contador_pecas = 0
+            print("Turno resetado com sucesso. Contadores zerados.")
+            time.sleep(0.3)
+
+    time.sleep(0.05)
